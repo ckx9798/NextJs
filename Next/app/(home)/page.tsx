f36d5dd6f925034de7981f5bea113720541bf52b
ckx9798 <ckx12@Naver.com>
2024-10-15 08:01:16 +0900

    Feat 리액트방식의 fetch

--- Next/app/(home)/page.tsx
+++ Next/app/(home)/page.tsx
@@ -1,11 +1,24 @@
-export const metadata = {
-  title: "Home",
-};
+"use client";
+
+import { useEffect, useState } from "react";
+
+// export const metadata = {
+//   title: "Home",
+// };
 
 export default function Page() {
-  return (
-    <>
-      <h1> Home </h1>
-    </>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://nomad-movies.nomadcoders.workers.dev/movies"
+    );
+    const json = await response.json();
+    setMovies(json);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  return <div>{isLoading ? "Loading..." : JSON.stringify(movies)}</div>;
 }
