b9e949e0f1ddc0543d14f0b27b7abdc8e9e7b33d
ckx9798 <ckx12@Naver.com>
2024-10-15 17:59:35 +0900

    Feat fetch , 병령 fetch 구현

--- Next/app/(movies)/movies/[id]/page.tsx
+++ Next/app/(movies)/movies/[id]/page.tsx
@@ -1,3 +1,24 @@
-export default function MovieDetail({ params: { id } }) {
-  return <div>MovieDetail {id} </div>;
+import { API_URL } from "../../../(home)/page";
+
+async function getMovie(id: string) {
+  console.log(`Fetching movies: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
+}
+
+async function getVideos(id: string) {
+  console.log(`Fetching videos: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
+}
+
+export default async function MovieDetail({ params: { id } }) {
+  console.log("start fetching");
+  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+  // const movie = await getMovie(id);
+  // const videos = await getVideos(id);
+  console.log("end fetching");
+  return <div>{movie.title} </div>;
 }
